3c9f681a43766af657b014312ae06a326812c5d5
Huy Tran <huy.tran@blazorschool.com>
2025-04-07 09:31:13 +0700

    Update component communication example

---
+++ ComponentCommunication/Components/SingleSourceOfTrurth/BlazorSchoolTransferService.cs
@@ -0,0 +1,20 @@
+﻿namespace ComponentCommunication.Components.SingleSourceOfTrurth;
+
+// This class is registered as a service in the Program.cs file.
+public class BlazorSchoolTransferService
+{
+    private string _helloMessage;
+
+    public string HelloMessage
+    {
+        get => _helloMessage; 
+        
+        set
+        {
+            _helloMessage = value;
+            HelloMessageChanged?.Invoke(this, value);
+        }
+    }
+
+    public event EventHandler<string> HelloMessageChanged = (sender, value) => { };
+}
