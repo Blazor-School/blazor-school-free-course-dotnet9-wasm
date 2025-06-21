4d5001848bba2baaed313706435572f0594bdfd9
Huy Tran <huy.tran@blazorschool.com>
2025-06-21 10:24:26 +0700

    Format code

--- SingleSourceOfTruth/Program.cs
+++ SingleSourceOfTruth/Program.cs
@@ -11,7 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddCascadingValue(sp =>
 {
-    CascadingParameterSharedData value = new();
+    var value = new CascadingParameterSharedData();
     value.Source = new(value, false);
 
     return value.Source;
